811bd74d4d033e74bdc7f2538dfecc676d58a5ed
Irineu Antunes <irineu@irineuantunes.com>
2022-09-18 21:35:16 -0300

    little fixes

--- tracking-processor/__main__.py
+++ tracking-processor/__main__.py
@@ -1,15 +1,26 @@
 import asyncio
 import numpy as np
-import cvlib as cv
-import cv2 as opencv
-from cvlib.object_detection import draw_bbox
+import cv2
 from hachi_nio import HachiNIOClient
 import json
+import sys
+
+sys.path.append('/home/irineu/darknet/v4/darknet')
+
+import darknet
 
 loop = None
-addr = "irineuantunes.com"
+#addr = "irineuantunes.com"
+addr="192.168.45.5"
 
-print("hi")
+thresh = .25
+
+network, class_names, class_colors = darknet.load_network(
+    "/home/irineu/a/img.v4.cfg",
+    "/home/irineu/a/img.data",
+    "/home/irineu/a/backup/img_last.weights",
+    batch_size=1
+)
 
 def cmd_on_connect(ref):
     print("New Connection! " + str(ref.id))
@@ -22,19 +33,49 @@ def cmd_on_connect(ref):
 
     cmdConn.send({"transaction" : "auth"}, "123")
 
+
+def image_detection(image_or_path):
+    # Darknet doesn't accept numpy images.
+    # Create one with image we reuse for each detect
+
+    width = darknet.network_width(network)
+    height = darknet.network_height(network)
+    darknet_image = darknet.make_image(width, height, 3)
+
+    if type(image_or_path) == "str":
+        image = cv2.imread(image_or_path)
+    else:
+        image = image_or_path
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height),
+                               interpolation=cv2.INTER_LINEAR)
+
+    darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
+    darknet.free_image(darknet_image)
+    #image = darknet.draw_boxes(detections, image_resized, class_colors)
+    #return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
+    return detections
+
 def onProcess(frame, sentFrom):
     print("will process");
 
     frame = np.asarray(bytearray(frame), dtype="uint8")
-    frame = opencv.imdecode(frame, opencv.IMREAD_COLOR)
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-    bbox, label, conf = cv.detect_common_objects(frame)
-    for i, item in enumerate(label):
-        print(item)
+    detections = image_detection(frame)
+    output = []
 
-    print(sentFrom);
+    for label, confidence, bbox in detections:
+        print(label)
+        left, top, right, bottom = darknet.bbox2points(bbox)
+        output.append({
+            "label": label,
+            "confidence": confidence,
+            "points": [left, top, right, bottom]
+        })
 
-    cmdConn.send({"transaction" : "process", "to": sentFrom}, json.dumps(bbox))
+    cmdConn.send({"transaction" : "process", "to": sentFrom}, json.dumps(output))
 
 
 def cmd_on_data(header, message, ref):
